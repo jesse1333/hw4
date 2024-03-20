94935c71e61e35ba4f31d9f8d0cc0d41870abb67
Jesse Tzo <tzo@usc.edu>
2024-03-20 23:36:55 +0000

    Initial commit for hw4

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,59 +1,57 @@
 #ifndef RECCHECK
-//if you want to add any #includes like <iostream> you must do them here (before the next endif)
+// if you want to add any #includes like <iostream> you must do them here (before the next endif)
 
 #endif
 
 #include "equal-paths.h"
 using namespace std;
 
-
 // You may add any prototypes of helper functions here
-
 bool checker(Node *root, int level, int& leafLevel)
 {
-  // checks edge case
-  if (root == nullptr)
-  {
-      return true;
-  }
+    // checks edge case
+    if (root == nullptr)
+    {
+        return true;
+    }
 
-  // if at leaf node
-  if (root->right == nullptr && root->left == nullptr)
-  {
-      // on first case, we set the leafLevel for subsequent checking
-      if (leafLevel == 0)
-      {
-          leafLevel = level;
-          return true;
-      }
+    // if at leaf node
+    if (root->right == nullptr && root->left == nullptr)
+    {
+        // on first case, we set the leafLevel for subsequent checking
+        if (leafLevel == 0)
+        {
+            leafLevel = level;
+            return true;
+        }
 
-      else if (level == leafLevel)
-      {
-          return true;
-      }
+        else if (level == leafLevel)
+        {
+            return true;
+        }
 
-      else
-      {
-          return false;
-      }
-  }
+        else
+        {
+            return false;
+        }
+    }
 
-  // recursively checks each leaf
-  if (checker(root->left, level + 1, leafLevel) && checker(root->right, level + 1, leafLevel))
-  {
-      return true;
-  }
+    // recursively checks each leaf
+    if (checker(root->left, level + 1, leafLevel) && checker(root->right, level + 1, leafLevel))
+    {
+        return true;
+    }
 
-  else
-  {
-      return false;
-  }
+    else
+    {
+        return false;
+    }
 }
 
 bool equalPaths(Node *root)
 {
-  // Add your code below
-  int level = 0;
-  int leafLevel = 0;
-  return checker(root, level, leafLevel);
+    // Add your code below
+    int level = 0;
+    int leafLevel = 0;
+    return checker(root, level, leafLevel);
 }
